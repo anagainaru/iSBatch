b1ff77db5c8755a67df8d7b9d5703af9d294bdcc
Ana Gainaru <ana.gainaru@gmail.com>
2020-04-09 02:49:02 +0000

    The example to compute a sequence cost includes comparison to the optimal cost

--- script_examples/check_sequence_goodness.py
+++ script_examples/check_sequence_goodness.py
@@ -13,12 +13,23 @@ if __name__ == '__main__':
 
     data = np.loadtxt(file_name, delimiter=' ')
     assert (len(data) > train_size), "Training size exceeds the total dataset"
-    training = data[:train_size] + [max(data)]
+    training = list(data[:train_size]) + [max(data)]
+
+    # compute the requests based on the entire data
+    wl = rqs.ResourceEstimator(data)
+    cl = rqs.ClusterCosts(1, 0, 0)
+    sequence = wl.compute_request_sequence(cluster_cost=cl)
+    # cost value will be used as reference as optimal
+    cost_opt = wl.compute_sequence_cost(sequence, data, cluster_cost=cl)
+
+    print("Request sequence based on the entire dataset: %s Cost %.2f" % (
+        sequence, cost_opt))
 
     # compute the requests based on the training data
     wl = rqs.ResourceEstimator(training)
-    sequence = wl.compute_request_sequence()
-    cost = wl.compute_sequence_cost(sequence, data)
+    sequence = wl.compute_request_sequence(cluster_cost=cl)
+    cost = wl.compute_sequence_cost(sequence, data, cluster_cost=cl)
 
-    print("Request sequence based on training: %s" %(sequence))
-    print("Sequence cost: %s" %(cost))
+    print("Request sequence based on training: %s\n"\
+          "Sequence cost: %.2f (within %.2f%% of optimal)" % (
+              sequence, cost, (cost-cost_opt)*100/cost_opt))
