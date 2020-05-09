f6f4ef8dd046ed8efb1924efcdcb41bf761b55fb
Ana Gainaru <ana.gainaru@gmail.com>
2020-05-08 21:04:24 -0400

    hooks for the benchmarks

---
+++ benchmark/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+import os
+
+
+@pytest.hookimpl()
+def pytest_sessionstart(session):
+    os.remove("metrics.perf")
+
+
+@pytest.mark.hookwrapper
+def pytest_benchmark_group_stats(config, benchmarks, group_by):
+    for bench in benchmarks:
+        fname = ":".join([i.split("[")[0]
+                          for i in bench["name"].split("_")[1:]])
+        with open("metrics.perf", 'a') as fp:
+            fp.write("time %s %s %f\n" % (
+                fname,
+                ':'.join([str(bench["params"][i]) for i in bench["params"]]),
+                bench["mean"]))
+    yield
