ef1553d814f68e5bb6dc31cc9da062d629cb46d1
Ana Gainaru <ana.gainaru@gmail.com>
2020-06-11 21:16:21 +0000

    Update examples to be able to use the new memory structure

--- examples/use_dynamic_checkpointing.py
+++ examples/use_dynamic_checkpointing.py
@@ -2,23 +2,27 @@ import sys
 sys.path.append("..")
 import iSBatch as rqs
 import numpy as np
-import sys
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Usage: %s log_file" %(sys.argv[0]))
+    if len(sys.argv) < 3:
+        print("Usage: %s walltime_log_file memory_file" %(sys.argv[0]))
         exit()
 
     file_name = sys.argv[1]
     history = np.loadtxt(file_name, delimiter=' ')
+    memory_footprint = np.loadtxt(sys.argv[2], delimiter=',')
 
     # set the cluster cost model
-    check_model = rqs.DynamicCheckpointMemoryModel([(10, 0), (1, 12)])
+    check_model = rqs.StaticCheckpointMemoryModel(
+            checkpoint_cost=np.max(memory_footprint),
+            restart_cost=np.max(memory_footprint))
     cl_cost = rqs.ClusterCosts(checkpoint_memory_model=check_model)
     wl = rqs.ResourceEstimator(history,
                                CR_strategy=rqs.CRStrategy.AlwaysCheckpoint)
-    sequence = wl.compute_request_sequence()
+    sequence = wl.compute_request_sequence(cluster_cost=cl_cost)
     print("Request sequence (static checkpoint): %s" %(sequence))
     
+    check_model = rqs.DynamicCheckpointMemoryModel(memory_footprint)
+    cl_cost = rqs.ClusterCosts(checkpoint_memory_model=check_model)
     sequence = wl.compute_request_sequence(cluster_cost=cl_cost)
     print("Request sequence (dynamic checkpoint): %s" %(sequence))
